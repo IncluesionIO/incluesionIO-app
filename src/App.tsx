99e64acc8b78b0c3600df61e794e2bad23349f15
sdlrios97 <sdlrios@gmail.com>
2022-09-13 16:36:49 -0400

    initial react environment setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Admin Incluesion App
+          Admin Incluesion APP
         </p>
         <a
           className="App-link"
