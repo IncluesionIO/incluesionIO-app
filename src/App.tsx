0a192f2e8b7641deb0ec4f00045e0cfbbc3eebfc
sdlrios97 <sdlrios@gmail.com>
2022-09-16 12:14:14 -0400

    changed text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Admin Incluesion APP
+          Admin Incluesion Stinky
         </p>
         <a
           className="App-link"
