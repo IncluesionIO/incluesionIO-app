f33f88dd0fc878b93f3977c88a7700a11c88b303
Daniel Tejeda <daviddan1998@gmail.com>
2022-10-06 20:28:00 -0400

    form validation and handling 401

--- src/components/loginBox/loginBox.tsx
+++ src/components/loginBox/loginBox.tsx
@@ -1,53 +1,118 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import './loginBox.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./loginBox.css";
 
 const LoginBox = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [identifierError, setIdentifierError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
-    const handleSubmit = (event: { preventDefault: () => void; }) =>
+  useEffect(() =>
+  {
+    setPasswordError("")
+  }, [password])
+
+  useEffect(() =>
+  {
+    setIdentifierError("")
+  }, [username])
+
+  const formValidator = () => {
+    console.log(username, password)
+    let error = false
+    if(username.trim().length < 1)
     {
-        event.preventDefault()
-        axios.get("http://localhost:13000")
-        .then(response =>
-            {
-                const live = response.data
-                console.log(live)
-            })
+        setIdentifierError("Field cannot be empty!")
+        error = true
     }
 
-    const title = <h1 className="containerLoginBox-title-text">Log In</h1>
+    if(password.trim().length < 1)
+    {
+        setPasswordError('Field cannot be empty!')
+        error = true
+    }
+    return error
+  };
 
-    const emailError = '';
-    const passwordError = ''
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if(!formValidator())
+    {
+        axios.post("http://localhost:13000/auth/login", 
+        {
+            username,
+            password
+        }).then((response) => {
+          const live = response.data;
+          console.log(live);
+        })
+        .catch(err =>
+            {
+                if(err.response.status === 401)
+                {
+                    setErrorAlert("A user with the username and password combination does not exist.")
+                }
+            });
+    }
+  };
 
-    return (
-        <div className="containerLoginBox">
-            <div className="containerLoginBox-title">
-                {title}
-            </div>
-            <form onSubmit={handleSubmit} className={'formLogin'}>
-                <div>
-                    <input className={'userInput'} type={'text'} id='userIdentification' required></input>
-                    <label htmlFor="userIdentification" className="inputLabel">Email or Username</label>
-                    <span className="error-message" aria-live="polite">{emailError}</span>
-                </div>
-                <div>
-                    <input className={'userInput'} type={'password'} id='userPassword' required></input>
-                    <label htmlFor="userPassword" className="inputLabel" aria-label="Password">Password</label>
-                    <span className="error-message" aria-live="polite">{passwordError}</span>
-                </div>
-                <div className="formSettings">
-                    <input type={'checkbox'} id='userSignInPreference'></input>
-                    <label htmlFor="userSignInPreference">Keep me signed in</label>
-                    <Link to='/forgot-password'>Forgot password?</Link>
-                </div>
-                <div>
-                    <button className={'btnLogin'} type="submit">Log In</button>
-                </div>
-            </form>
+  const title = <h1 className="containerLoginBox-title-text">Log In</h1>;
+
+
+
+  return (
+    <div className="containerLoginBox">
+      <div className="containerLoginBox-title">{title}</div>
+      <span id="errorAlert" className="errorAlert">{errorAlert}</span>
+      <form onSubmit={handleSubmit} className={"formLogin"}>
+        <div>
+          <input
+            className={"userInput"}
+            type={"text"}
+            id="userIdentification"
+            onChange={(e) => {setUsername(e.target.value)}}
+          ></input>
+          <label htmlFor="userIdentification" className="inputLabel">
+            Username
+          </label>
+          <span className="error-message" aria-live="polite">
+            {identifierError}
+          </span>
         </div>
-    )
-}
+        <div>
+          <input
+            className={"userInput"}
+            type={"password"}
+            id="userPassword"
+            onChange={(e) => {setPassword(e.target.value)}}
+          ></input>
+          <label
+            htmlFor="userPassword"
+            className="inputLabel"
+            aria-label="Password"
+          >
+            Password
+          </label>
+          <span className="error-message" aria-live="polite">
+            {passwordError}
+          </span>
+        </div>
+        <div className="formSettings">
+          <input type={"checkbox"} id="userSignInPreference"></input>
+          <label htmlFor="userSignInPreference">Keep me signed in</label>
+          <Link to="/forgot-password">Forgot password?</Link>
+        </div>
+        <div>
+          <button className={"btnLogin"} type="submit">
+            Log In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export {LoginBox}
+export { LoginBox };
