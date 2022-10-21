e2cc3f5609144ec9607d4f29488e9dc6f330f6f6
Daniel Tejeda <daviddan1998@gmail.com>
2022-10-21 15:16:00 -0400

    integrated login action

--- src/components/adminHome/adminHome.tsx
+++ src/components/adminHome/adminHome.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from 'react-router-dom'
 import './adminHome.css'
 import { AdminTopBar } from "./adminTopBar/adminTopBar";
 import { AdminMain } from "./adminMain/adminMain";
 
 const AdminHome = () => {
+    const [userId, setUserId] = useState(() =>
+    {
+        const saved = localStorage.getItem('userId') || ""
+        const value = JSON.parse(saved)
+        return value || ""
+    })
+
+    if(!userId.length)
+    {
+        return <Navigate to='/' replace />
+    }
+
     return (
         <div className="admin">
             <AdminTopBar />
