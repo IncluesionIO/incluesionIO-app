94b231a1cf51aaf799c505d3ab66211d1b22e326
Daniel Tejeda <daviddan1998@gmail.com>
2022-11-14 23:30:50 -0500

    fixed render issue

--- src/components/adminApp/adminAnalyticsView/AnalyticsContainer/AnalyticsContainer.tsx
+++ src/components/adminApp/adminAnalyticsView/AnalyticsContainer/AnalyticsContainer.tsx
@@ -1,61 +1,59 @@
-import axios from 'axios'
-import react, { useEffect, useState } from 'react'
-import BarChart from '../../../graphs/barChart'
-import getLifetimeAverageAnswerPerQuestion from '../../../graphs/filterMethods/getLifetimeAverageAnswerPerQuestion'
-import './AnalyticsContainer.css'
-import DataRow from './DataRow/DataRow'
+import axios from "axios";
+import react, { useEffect, useState, useReducer } from "react";
+import BarChart from "../../../graphs/barChart";
+import getLifetimeAverageAnswerPerQuestion from "../../../graphs/filterMethods/getLifetimeAverageAnswerPerQuestion";
+import "./AnalyticsContainer.css";
+import DataRow from "./DataRow/DataRow";
 
-const AnalyticsContainer = (props:any) =>
-{
-  const [assessmentList, setAssessmentList] = useState([])
-  const [questionAverages, setQuestionAverages] = useState<any[]>([])
-  
-  useEffect(() =>
-  {
-    axios.get('http://localhost:13000/assessment/list')
-    .then(response =>
-      {
+const AnalyticsContainer = (props: any) => {
+  const [assessmentList, setAssessmentList] = useState([]);
+  const [questionAverages, setQuestionAverages] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:13000/assessment/list")
+      .then((response) => {
         setAssessmentList(response.data)
         setQuestionAverages(getLifetimeAverageAnswerPerQuestion(assessmentList, null))
       })
-    .catch(err =>
-      {
-        console.log(err)
-      })
-  }, [])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [questionAverages]);
 
-  const getQuestionAveragesLists = () =>
-  {
-    return questionAverages.map(values =>
-    {
-      return <DataRow key={values[0]} question={values[0]} answerAverage={values[1]} />
-    })
-  }
+  const getQuestionAveragesLists = () => {
+    console.log(questionAverages)
+    return questionAverages.map((values) => {
+      return (
+        <DataRow
+          key={values[0]}
+          question={values[0]}
+          answerAverage={values[1]}
+        />
+      );
+    });
+  };
 
   return (
-    <div className='analytics-container'>
-      <div className='analytics-container-left'>
-        <div className='graph-group'>
-          <div className='chart'>
-            <BarChart dataset={assessmentList}/>
+    <div className="analytics-container">
+      <div className="analytics-container-left">
+        <div className="graph-group">
+          <div className="chart">
+            <BarChart dataset={assessmentList} />
           </div>
-          <div className='chart'>
-            <BarChart dataset={assessmentList}/>
+          <div className="chart">
+            <BarChart dataset={assessmentList} />
           </div>
-          <div className='chart'>
-            <BarChart dataset={assessmentList}/>
+          <div className="chart">
+            <BarChart dataset={assessmentList} />
           </div>
         </div>
         <h3>Assessment Question Metrics</h3>
-        <div className="data-rows-container">
-          {getQuestionAveragesLists()}
-        </div>
+        <div className="data-rows-container">{getQuestionAveragesLists()}</div>
       </div>
-      <div className='analytics-container-right'>
-        Hello Right
-      </div>
+      <div className="analytics-container-right">Hello Right</div>
     </div>
-  )
-}
+  );
+};
 
-export default AnalyticsContainer
+export default AnalyticsContainer;
